402854f74c01fbc0eb2587e953a291aab9def95e
y,fenyuk <y.fenyuk@levi9.com>
2016-02-12 17:17:15 +0200

    phantom

--- index.js
+++ index.js
@@ -6,11 +6,25 @@ var mustache = require('mustache');
 var AWS = require('aws-sdk');
 var moment = require('moment');
 var exec = require('child-process-promise').exec;
+var path = require('path');
+var childProcess = require('child_process');
+var phantomjs = require('phantomjs');
+var binPath = phantomjs.path;
 
 exports.handler = function (event, context) {
     request(event.webpage, function (err, response, body) {
         if (err) console.log(err, err.stack); // an error occurred
 
+var childArgs = [
+  path.join(__dirname, 'phantomjs-script.js'),
+  'some other argument (passed to phantomjs script)'
+]
+
+childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+  // handle results
+});
+
+/*
         exec('echo hello; echo again hello')
             .then(function (result) {
                 var stdout = result.stdout;
@@ -26,7 +40,7 @@ exports.handler = function (event, context) {
             .progress(function (childProcess) {
                 console.log('childProcess.pid: ', childProcess.pid);
             });
-
+*/
      
                 
         console.log("jsPDF done!");
