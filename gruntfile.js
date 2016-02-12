137e287f695efa726d598f72208d5ca5fd280981
y,fenyuk <y.fenyuk@levi9.com>
2016-02-12 16:03:58 +0200

    added js

---
+++ gruntfile.js
@@ -0,0 +1,20 @@
+var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-aws-lambda');
+
+grunt.initConfig({
+    lambda_invoke: {
+        default: {
+        }
+    },
+    lambda_deploy: {
+        default: {
+            arn: 'arn:aws:lambda:eu-west-1:162782911101:function:link-scrapper'
+        }
+    },
+    lambda_package: {
+        default: {
+        }
+    }
+});
+
+grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy']);
---
+++ index.js
@@ -0,0 +1,33 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var url = require('url');
+var fs = require('fs');
+var mustache = require('mustache');
+var AWS = require('aws-sdk');
+var moment = require('moment');
+var exec = require('child-process-promise').exec;
+
+exports.handler = function (event, context) {
+    request(event.webpage, function (err, response, body) {
+        if (err) console.log(err, err.stack); // an error occurred
+
+        exec('echo hello')
+            .then(function (result) {
+                var stdout = result.stdout;
+                var stderr = result.stderr;
+                console.log('stdout: ', stdout);
+                console.log('stderr: ', stderr);
+            })
+            .fail(function (err) {
+                console.error('ERROR: ', err);
+            })
+            .progress(function (childProcess) {
+                console.log('childProcess.pid: ', childProcess.pid);
+            });
+
+     
+                
+        console.log("jsPDF done!");
+        });
+     
+};
